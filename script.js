66358e7d2833c40253e9f62e7a5c5712e22d7d1e
Batir20 <102732982+Batir20@users.noreply.github.com>
2023-11-01 15:17:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+elements = document.querySelectorAll(".table__elem");
+
+
+for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", function() {
+        for (let x = 0; x < elements.length; x++) {
+            elements[x].classList.remove("active");            
+        }
+        elements[i].classList.toggle("active");
+    })
+}
